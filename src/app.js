e7e654a996ee91d1f3ff0308068e64b900c424e2
Ribeiro1300 <jhonatan_dribeiro@hotmail.com>
2021-11-06 18:29:02 -0300

    setting deploy part 2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { postUser, login, deleteCurrentSession } from "./controllers/users.js";
-import { getRecords, postRecords } from "./controllers/records.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 
 export default app;
