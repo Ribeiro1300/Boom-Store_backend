f952fe5a5b2cfe1852098e4ba3e8ba004d60db74
fernandollisboa <fernando.costa@ccc.ufcg.edu.br>
2021-11-15 06:55:36 -0300

    add: final signup e login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -33,7 +33,10 @@ async function login(req, res) {
 				user.id,
 			]);
 		}
-		return res.send({ token }).status(200);
+
+		const userName = emailCheck.rows[0].name;
+		const id = emailCheck.rows[0].id;
+		return res.send({ userName, id, token }).status(200);
 	} catch (err) {
 		console.log(err);
 		return res.sendStatus(500);
